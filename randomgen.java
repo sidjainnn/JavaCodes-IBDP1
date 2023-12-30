6a0b0a38f7f857136493353b4e04febf41899100
sidjainnn <155260456+sidjainnn@users.noreply.github.com>
2023-12-31 00:54:48 +0530

    Add files via upload

---
+++ randomgen.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+
+public class randomgen {
+
+	public static void main(String[] args) {
+		
+		Random number = new Random();
+		int x = number.nextInt();
+		
+		System.out.println(x); 
+
+	}
+
+}
