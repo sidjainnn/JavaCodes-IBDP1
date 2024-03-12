bc63015e2b9956e283d144eb128c0517af858b72
sidjainnn <155260456+sidjainnn@users.noreply.github.com>
2024-03-12 19:35:57 +0530

    Add files via upload

---
+++ BioData/src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		String Name; 
+		Name = "Sidharth Jain";
+		String School; 
+		School = "The Shriram Millenium School"; 
+		String std; 
+		std = "11th";
+		String sub; 
+		sub = "Computer";
+		String city = "Noida"; 
+		
+		System.out.println("Name:" +Name); 
+		System.out.println("School:" +School); 
+		System.out.println("Class:" +std); 
+		System.out.println("Subject :" +sub); 
+		System.out.println("City:" +city); 
+		
+		
+		
+	}
+
+}
